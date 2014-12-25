3550bedd2a59c281cf9edd9723eb1d9cb08b5276
ead <ead.fritz@gmail.com>
2014-12-25 16:45:38 +0100

    Made it possible to run tests on *duinos without USB Keyboard  mapping

--- MythIr.ino
+++ MythIr.ino
@@ -8,19 +8,59 @@
  */
 
 #include <IRremote.h>
-#include <USBAPI.h>
-
-
 
 // Comment out this line for serial debugging:
-//#define USE_KEYBOARD
-
+#define USE_KEYBOARD
 
 
 #ifndef USE_KEYBOARD
   #define USE_SERIAL
 #else 
   #undef USE_SERIAL
+#endif
+
+
+#ifndef USE_KEYBOARD
+// these values are copied from arduino/USBAPI.h
+// An ordinary UNO can't access these values even if USBAPI.h is included (at least if we don't mess with system #defines like USBCON)
+
+#define KEY_LEFT_CTRL           0x80
+#define KEY_LEFT_SHIFT          0x81
+#define KEY_LEFT_ALT            0x82
+#define KEY_LEFT_GUI            0x83
+#define KEY_RIGHT_CTRL          0x84
+#define KEY_RIGHT_SHIFT         0x85
+#define KEY_RIGHT_ALT           0x86
+#define KEY_RIGHT_GUI           0x87
+
+#define KEY_UP_ARROW            0xDA
+#define KEY_DOWN_ARROW          0xD9
+#define KEY_LEFT_ARROW          0xD8
+#define KEY_RIGHT_ARROW         0xD7
+#define KEY_BACKSPACE           0xB2
+#define KEY_TAB                 0xB3
+#define KEY_RETURN              0xB0
+#define KEY_ESC                 0xB1
+#define KEY_INSERT              0xD1
+#define KEY_DELETE              0xD4
+#define KEY_PAGE_UP             0xD3
+#define KEY_PAGE_DOWN           0xD6
+#define KEY_HOME                0xD2
+#define KEY_END                 0xD5
+#define KEY_CAPS_LOCK           0xC1
+#define KEY_F1                  0xC2
+#define KEY_F2                  0xC3
+#define KEY_F3                  0xC4
+#define KEY_F4                  0xC5
+#define KEY_F5                  0xC6
+#define KEY_F6                  0xC7
+#define KEY_F7                  0xC8
+#define KEY_F8                  0xC9
+#define KEY_F9                  0xCA
+#define KEY_F10                 0xCB
+#define KEY_F11                 0xCC
+#define KEY_F12                 0xCD
+
 #endif
 
 static const int RECV_PIN = 2;
@@ -305,8 +345,8 @@ void setup()
 {
 #ifdef USE_SERIAL  
   Serial.begin(9600);
-  delay(1000);
-  virtualSerialPrintln("Running in serial mode - no key presses will be generated.");
+  delay(500);
+  Serial.println("Running in serial mode - no key presses will be generated.");
 #endif
 #ifdef USE_KEYBOARD
   Keyboard.begin();
